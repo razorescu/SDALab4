bd4b7ab4414ce93a4ee47b101186807f6ea164bd
Catana Victor <victor.cristian12@yahoo.com>
2020-05-19 19:09:36 +0300

    Added files

---
+++ SDALab4/P12.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+typedef int TElem;
+typedef bool(*Relation) (TElem e1, TElem e2);
+
+//returns a vector with the first K elements (considering a relation) of the vector
+//if k is less than or equal to 0 an exception will be thrown.
+vector<TElem> firstK(vector<TElem> inputVector, Relation r, int k);
